e8a0663306231f08a3ecc47dce5721f15453aae1
vncz14 <57498723+vncz14@users.noreply.github.com>
2023-07-27 17:47:37 -0400

    save the settings store locally

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 import App from './App.vue'
 
 const pinia = createPinia()
-const app = createApp(App)
+pinia.use(piniaPluginPersistedstate)
 
+const app = createApp(App)
 app.use(pinia)
 app.mount('#app')
